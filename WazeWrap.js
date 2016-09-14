2c445194811512e93393d7fc35aa2fda972b7021
justins83 <justins83.waze@gmail.com>
2016-09-14 11:19:58 -0400

    Initial import of the script

---
+++ WazeWrap.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         WazeWrap
+// @namespace    
+// @version      0.1
+// @description  A library to wrap the Waze API
+// @author       JustinS83/MapOMatic
+// @include      https://beta.waze.com/*editor/*
+// @include      https://www.waze.com/*editor/*
+// @exclude      https://www.waze.com/*user/editor/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
