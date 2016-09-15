916a83efe047431ea3078431367b21d1d4e7affc
justins83 <justins83.waze@gmail.com>
2016-09-15 12:12:15 -0400

    Adding basic framework
    Adding a Model section with some basic functionality for pulling IDs &
    Names

--- WazeWrap.js
+++ WazeWrap.js
@@ -13,5 +13,47 @@
 (function() {
     'use strict';
 
-    // Your code here...
+
+
+    function init(){
+
+        var Model = new Model;
+
+        
+    };
+
+    function Model(){
+
+        this.getPrimaryStreetID = function(segmentID){
+                return W.model.segments.get(segmentID).attributes.primaryStreetID;
+                };
+
+        this.getCityID() = function(primaryStreetID){
+            return W.model.streets.get(primaryStreetID).cityID;
+        };
+
+        this.getCityName() = function(primaryStreetID){
+            return W.model.cities.get(getCityID(primaryStreetID)).attributes.Name;
+        };
+
+        this.getStateName() = function(primaryStreetID){
+            W.model.states.get(getStateID(primaryStreetID)).Name;   
+        };
+
+        this.getStateID() = function(primaryStreetID){
+            return W.model.cities.get(primaryStreetID).attributes.stateID;
+        };
+
+        this.getCountryID() = function(primaryStreetID){
+            return W.model.cities.get(getCityID(primaryStreetID)).attributes.CountryID;
+        };
+
+        this.getCountryName() = function(primaryStreetID){
+            return W.model.countries.get(getCountryID(primaryStreetID)).name;
+        };
+
+    };
+
+
+
 })();
