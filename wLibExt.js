7968cc3c9c0c796e5715a07c6ab0569c7fb8e0f6
justins83 <justins83.waze@gmail.com>
2016-09-28 15:13:59 -0400

    Update
    Adding Geometry section with 2 conversion methods & some Roundabout methods in the Model section.

--- wLibExt.js
+++ wLibExt.js
@@ -11,12 +11,10 @@
 // @grant        none
 // ==/UserScript==
 
-/* global W,wLib */
-
 (function() {
     'use strict';
 
-    function bootstrap(tries) {
+function bootstrap(tries) {
         tries = tries || 1;
         if (window.wLib) {
             init();
@@ -32,31 +30,83 @@
     function init(){
         console.log('wLibExt Loaded');
         extendModel();
-    }
+        extendGeometry();
+    };
+
+    function extendGeometry(){
+        var geometry = wLib.Geometry;
+
+        //Converts to "normal" GPS coordinates
+        geometry.ConvertTo4326 = function (long, lat){
+            var projI=new OpenLayers.Projection("EPSG:900913");
+            var projE=new OpenLayers.Projection("EPSG:4326");
+            return (new OpenLayers.LonLat(long, lat)).transform(projI,projE);
+        };
+    
+        geometry.ConvertTo900913 = function (long, lat){
+            var projI=new OpenLayers.Projection("EPSG:900913");
+            var projE=new OpenLayers.Projection("EPSG:4326");
+            return (new OpenLayers.LonLat(long, lat)).transform(projE,projI);
+        };
+    };
 
     function extendModel(){
         var model = wLib.Model;
+
         model.getPrimaryStreetID = function(segmentID){
             return W.model.segments.get(segmentID).attributes.primaryStreetID;
         };
+
+        model.getStreetName = function(primaryStreetID){
+            return W.model.streets.get(PrimaryStreetID).name;
+        };
+
         model.getCityID = function(primaryStreetID){
             return W.model.streets.get(primaryStreetID).cityID;
         };
+
         model.getCityName = function(primaryStreetID){
-            return W.model.cities.get(getCityID(primaryStreetID)).attributes.Name;
+            return W.model.cities.get(model.getCityID(primaryStreetID)).attributes.Name;
         };
+
         model.getStateName = function(primaryStreetID){
-            return W.model.states.get(getStateID(primaryStreetID)).Name;
+            W.model.states.get(getStateID(primaryStreetID)).Name;   
         };
+
         model.getStateID = function(primaryStreetID){
             return W.model.cities.get(primaryStreetID).attributes.stateID;
         };
+
         model.getCountryID = function(primaryStreetID){
-            return W.model.cities.get(getCityID(primaryStreetID)).attributes.CountryID;
+            return W.model.cities.get(model.getCityID(primaryStreetID)).attributes.CountryID;
         };
+
         model.getCountryName = function(primaryStreetID){
             return W.model.countries.get(getCountryID(primaryStreetID)).name;
         };
-    }
+
+        model.getCityNameFromSegmentObj = function(segObj){
+            return getCityName(segObj.attributes.primaryStreetID);
+        };
+
+        model.getStateNameFromSegmentObj = function(segObj){
+            return getStateName(segObj.attributes.primaryStreetID);
+        };
+
+        model.getAllRoundaboutSegmentsFromObj = function(segObj){
+            var originalSegID = segObj.model.attributes.id;
+            var RASegments = new Array;
+
+            
+        };
+
+        model.isRoundaboutSegment = function(segmentID){
+            if(W.model.segments.get(segmentID).attributes.junctionID === null)
+                return false;
+            else
+                return true;
+        };
+
+    };
 
 })();
