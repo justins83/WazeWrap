5ef30d548e4155dce9e867bad46a6eac81cfbe3b
justins83 <justins83.waze@gmail.com>
2017-11-10 10:54:14 -0500

    Update WazeWrap.js

--- WazeWrap.js
+++ WazeWrap.js
@@ -1231,7 +1231,10 @@ var WazeWrap = {};
 
                     this.appendTab();
 	    var that = this;
-		    
+		    if (Waze.prefs) {
+		        Waze.prefs.on('change:isImperial', function(){that.appendTab(that);});
+		    }
+		    Waze.app.modeController.model.bind('change:mode', that.appendTab);
                 }
             },
 
