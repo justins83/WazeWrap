ad6493a687972d671609e007628f0a9528be8b08
justins83 <justins83.waze@gmail.com>
2018-01-24 08:53:32 -0500

    Update WazeWrap.js

--- WazeWrap.js
+++ WazeWrap.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WazeWrapBeta
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2018.01.23.01
+// @version      2018.01.24.01
 // @description  A base library for WME script writers
 // @author       JustinS83/MapOMatic
 // @include      https://beta.waze.com/*editor*
