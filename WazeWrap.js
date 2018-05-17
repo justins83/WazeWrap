d9d402f6d12c14dd35ce763ebd711021640d62f3
justins83 <justins83.waze@gmail.com>
2018-05-17 12:48:04 -0400

    Update WazeWrap.js

--- WazeWrap.js
+++ WazeWrap.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WazeWrapBeta
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2018.05.17.01
+// @version      2018.05.17.02
 // @description  A base library for WME script writers
 // @author       JustinS83/MapOMatic
 // @include      https://beta.waze.com/*editor*
@@ -13,7 +13,7 @@
 /* global W */
 /* global WazeWrap */
 
-  var WazeWrap = {Ready: false, Version: "2018.05.15.03"};
+  var WazeWrap = {Ready: false, Version: "2018.05.17.02"};
 
 (function() {
     'use strict';
@@ -79,7 +79,7 @@
 		return W.selectionManager.select(featureArray);
 	}
 	
-	WazeWrap.hasPlaceSelected() = function(){
+	WazeWrap.hasPlaceSelected = function(){
 		if(W.selectionManager.hasSelectedFeatures() && W.selectionManager.getSelectedFeatures()[0].model.type === "venue")
 			return true;
 		else
