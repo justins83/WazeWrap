7fc69464e3fa5a5b1638f5eb3062d01bb559859a
justins83 <justins83.waze@gmail.com>
2019-05-01 11:40:10 -0400

    Update WazeWrap.js
    Setting the WW version from the header.

--- WazeWrap.js
+++ WazeWrap.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WazeWrapBeta
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2019.05.01.01
+// @version      2019.05.01.02
 // @description  A base library for WME script writers
 // @author       JustinS83/MapOMatic
 // @include      https://beta.waze.com/*editor*
@@ -38,6 +38,7 @@
 
     function init(){
         console.log("WazeWrap initializing...");
+	    WazeWrap.Version = GM_info.script.version;
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 
         //SetUpRequire();
